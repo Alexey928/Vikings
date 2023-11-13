cff9a6eb1f1dd891ce29aea80b595c6230778bea
Alexey928 <aleksey__tihakov@ukr.net>
2023-11-13 20:00:28 +0300

    progress 0.0.5

--- src/gameOOPModel/figures/Figure.ts
+++ src/gameOOPModel/figures/Figure.ts
@@ -1,6 +1,7 @@
 import {Colors} from "../Colors";
 import {Cell} from "../cell";
 import {useId} from "react";
+import Logo from "../../../public/King.png"
 
 
 export enum FifureNames{
@@ -11,14 +12,14 @@ export enum FifureNames{
 
 export class Figure{
     colors:Colors;
-    logo:null;
+    logo: typeof Logo;
     cell:Cell;
     name:string;
     id:string;
 
     constructor(colors: Colors, cell: Cell) {
         this.colors = colors;
-        this.logo = null;
+        this.logo = "";
         this.cell = cell;
         this.name = FifureNames.FIGURE
         this.id = useId();
--- src/gameOOPModel/figures/pawn.ts
+++ src/gameOOPModel/figures/pawn.ts
@@ -1,10 +1,14 @@
-import {Figure} from "./Figure";
+import {FifureNames, Figure} from "./Figure";
 import {Colors} from "../Colors";
 import {Cell} from "../cell";
+import blackPawn from "../../../public/blackPawn.png"
+import whitePawn from "../../../public/whitePawn.png"
 
 export class Pawn extends Figure{
   constructor(colors: Colors, cell: Cell) {
     super(colors, cell);
+    this.logo = colors === Colors.BLACK?blackPawn:whitePawn;
+    this.name = FifureNames.PAWN
   }
 
 
